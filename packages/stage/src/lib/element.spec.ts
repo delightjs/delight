a1639c0fe61fab5352065e5a7428a8fa622c4d11
蒼時弦也 <elct9620@frost.tw>
2023-02-10 18:54:33 +0800

    Fix createElement spec to use async mode

--- packages/stage/src/lib/element.spec.ts
+++ packages/stage/src/lib/element.spec.ts
@@ -3,13 +3,13 @@ import { Container } from 'pixi.js';
 import { createElement } from './element';
 
 describe('createElement', () => {
-  it('should contains a container', () => {
-    const element = createElement(Container, null);
+  it('should contains a container', async () => {
+    const element = await createElement(Container, null);
     expectTypeOf(element).toMatchTypeOf<Container>();
   });
 
-  it('should add children', () => {
-    const element = createElement(
+  it('should add children', async () => {
+    const element = await createElement(
       Container,
       null,
       createElement(Container, null),
